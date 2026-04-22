e222ce417332531c370b2487449a989240a58396
vishnu143186 <mm1475@srmist.edu.in>
2026-04-22 10:58:08 +0530

    Refactoring Unit Enum to Standalone

--- app/src/MainTest.java
+++ app/src/MainTest.java
@@ -7,57 +7,52 @@ public class MainTest {
 
     @Test
     void testAddition_TargetUnit_Feet() {
-        var f = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var i = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        var f = new QuantityMeasurementApp.Quantity(1.0, LengthUnit.FEET);
+        var i = new QuantityMeasurementApp.Quantity(12.0, LengthUnit.INCH);
 
-        var result = QuantityMeasurementApp.Quantity.add(f, i,
-                QuantityMeasurementApp.LengthUnit.FEET);
+        var result = QuantityMeasurementApp.Quantity.add(f, i, LengthUnit.FEET);
 
-        assertTrue(result.equals(new QuantityMeasurementApp.Quantity(2.0,
-                QuantityMeasurementApp.LengthUnit.FEET)));
+        assertTrue(result.equals(new QuantityMeasurementApp.Quantity(2.0, LengthUnit.FEET)));
     }
 
     @Test
     void testAddition_TargetUnit_Inches() {
-        var f = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var i = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        var f = new QuantityMeasurementApp.Quantity(1.0, LengthUnit.FEET);
+        var i = new QuantityMeasurementApp.Quantity(12.0, LengthUnit.INCH);
 
-        var result = QuantityMeasurementApp.Quantity.add(f, i,
-                QuantityMeasurementApp.LengthUnit.INCH);
+        var result = QuantityMeasurementApp.Quantity.add(f, i, LengthUnit.INCH);
 
-        assertTrue(result.equals(new QuantityMeasurementApp.Quantity(24.0,
-                QuantityMeasurementApp.LengthUnit.INCH)));
+        assertTrue(result.equals(new QuantityMeasurementApp.Quantity(24.0, LengthUnit.INCH)));
     }
 
     @Test
     void testAddition_TargetUnit_Yards() {
-        var f = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var i = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        var f = new QuantityMeasurementApp.Quantity(1.0, LengthUnit.FEET);
+        var i = new QuantityMeasurementApp.Quantity(12.0, LengthUnit.INCH);
 
-        var result = QuantityMeasurementApp.Quantity.add(f, i,
-                QuantityMeasurementApp.LengthUnit.YARD);
+        var result = QuantityMeasurementApp.Quantity.add(f, i, LengthUnit.YARD);
 
-        assertEquals(0.666666, result.toString().contains("YARD") ? 0.666666 : 0.0, 1e-3);
+        // Better assertion (numeric instead of string)
+        assertEquals(0.666666,
+                result.equals(new QuantityMeasurementApp.Quantity(0.666666, LengthUnit.YARD)) ? 0.666666 : 0.0,
+                1e-3);
     }
 
     @Test
     void testAddition_Commutativity_WithTargetUnit() {
-        var a = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var b = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        var a = new QuantityMeasurementApp.Quantity(1.0, LengthUnit.FEET);
+        var b = new QuantityMeasurementApp.Quantity(12.0, LengthUnit.INCH);
 
-        var r1 = QuantityMeasurementApp.Quantity.add(a, b,
-                QuantityMeasurementApp.LengthUnit.YARD);
-
-        var r2 = QuantityMeasurementApp.Quantity.add(b, a,
-                QuantityMeasurementApp.LengthUnit.YARD);
+        var r1 = QuantityMeasurementApp.Quantity.add(a, b, LengthUnit.YARD);
+        var r2 = QuantityMeasurementApp.Quantity.add(b, a, LengthUnit.YARD);
 
         assertTrue(r1.equals(r2));
     }
 
     @Test
     void testAddition_NullTargetUnit() {
-        var f = new QuantityMeasurementApp.Quantity(1.0, QuantityMeasurementApp.LengthUnit.FEET);
-        var i = new QuantityMeasurementApp.Quantity(12.0, QuantityMeasurementApp.LengthUnit.INCH);
+        var f = new QuantityMeasurementApp.Quantity(1.0, LengthUnit.FEET);
+        var i = new QuantityMeasurementApp.Quantity(12.0, LengthUnit.INCH);
 
         assertThrows(IllegalArgumentException.class, () -> {
             QuantityMeasurementApp.Quantity.add(f, i, null);
